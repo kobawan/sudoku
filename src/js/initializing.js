77590e3b7b82d90441b8cbac033e659531e90da3
Sara Nordmyr da Cunha <snordmyrdacunha@evolutiongaming.com>
2018-03-03 19:02:52 +0200

    Fix coordinates misalignment and font

--- src/js/initializing.js
+++ src/js/initializing.js
@@ -4,8 +4,7 @@ import {
 	addTableCellListeners,
 } from "./game/game";
 
-// eslint-disable-next-line no-unused-vars
-import styles from "../styles";
+import "../styles";
 
 // Initialize menu event listeners
 addMenuButtonListeners();
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,9 +1,6 @@
-/* eslint-disable no-unused-vars*/
-import buttonStyles from "./buttons.less";
-import cellStyles from "./cell-states.less";
-import gameStyles from "./game.less";
-import lobbyStyles from "./lobby.less";
-import commonStyles from "./common.less";
-import sideMenuStyles from "./side-menu.less";
-import popupStyles from "./popup.less";
-/* eslint-enable no-unused-vars*/
+import "./buttons.less";
+import "./cell-states.less";
+import "./game.less";
+import "./lobby.less";
+import "./common.less";
+import "./side-menu.less";
