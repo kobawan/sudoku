e18f598d17056632e6a00b6814870cea2400d1fc
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2018-10-08 20:57:40 +0300

    Fix missing table borders when scaling

--- src/js/components/sudoku-table/Cell.tsx
+++ src/js/components/sudoku-table/Cell.tsx
@@ -4,31 +4,18 @@ import "./cell.less";
 
 import { CellMode, CellProps } from "../../consts";
 
-export enum CellClassType {
-    READONLY = "readOnly",
-    PENCIL = "pencil",
-    NOTES = "notes",
-    HIGHLIGHT = "highlight",
-    ERROR = "error",
-}
-
 export const Cell = (props: CellProps) => {
-    let className: CellClassType = CellClassType.PENCIL;
-    if (props.mode === CellMode.Notes) {
-        className = CellClassType.NOTES;
-    } else if (props.mode === CellMode.ReadOnly) {
-        className = CellClassType.READONLY;
-    }
-
     return (
         <textarea
             readOnly={props.mode === CellMode.ReadOnly}
-            maxLength={props.mode !== CellMode.Notes ? 1 : 9}
+			maxLength={props.mode !== CellMode.Notes ? 1 : 9}
+			rows= {1}
+			cols={1}
             value={props.value || ""}
             className={[
-                className,
-                props.withError && props.mode !== CellMode.Notes ? CellClassType.ERROR : null,
-                props.withHighlight && props.mode !== CellMode.Notes ? CellClassType.HIGHLIGHT : null,
+				props.mode,
+                props.withError && props.mode !== CellMode.Notes ? "error" : null,
+                props.withHighlight && props.mode !== CellMode.Notes ? "highlight" : null,
             ].join(" ")}
             onKeyUp={props.onKeyup}
             onFocus={props.onFocus}
